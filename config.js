9e76ecab062e9ba35a216b48e26809c2b6056e81
ngochai20112003-del <ngochai20112003@gmail.com>
2026-03-08 11:37:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const eventName = 'Chúc Mừng 8/3';
 const titleCard = 'Tặng Em nhé';
 const contentCard = 'Chúc cô gái đáng yêu này một ngày 8/3 thật nhiều niềm vui, luôn cười thật tươi vì nụ cười của em rất đẹp.';
 
-const giftImage = 'hoa2.jpg';
+const giftImage = 'hoa3.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
